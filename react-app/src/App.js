769d417e6ea80a0b234251aaae4b9be87e1a840e
Stanley Wang <wangstanley910@gmail.com>
2024-08-02 18:53:34 -0400

    Initialize react-app

---
+++ react-app/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="flex flex-col min-h-screen bg-white">
+      {/* min-h-screen - app full height, flex-col - stack items vertically */}
+    </div>
+  );
+}
+
+export default App;
