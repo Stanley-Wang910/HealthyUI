d86ed4725b62f8bea6214f326a142b7e7afc3b42
Stanley Wang <wangstanley910@gmail.com>
2024-08-02 20:37:17 -0400

    Integration of Areebah's html & css into React HTMX

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,10 +1,15 @@
 import React from "react";
 import "./App.css";
 
+import Header from "./components/Header.js";
+import MainFeed from "./components/MainFeed.js";
+
 function App() {
   return (
     <div className="flex flex-col min-h-screen bg-white">
       {/* min-h-screen - app full height, flex-col - stack items vertically */}
+      <Header />
+      <MainFeed />
     </div>
   );
 }
