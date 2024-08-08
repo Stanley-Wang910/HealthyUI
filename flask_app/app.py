145d2d87fc8d407e3993023fafc843ee5a7575d8
Stanley Wang <wangstanley910@gmail.com>
2024-08-08 18:28:34 -0400

    Working integration of Google's Fact Check API with GO, with JSON Formatted strings returned by calling the Go Executable in python

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,5 +1,7 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
+import subprocess
+import shlex
 import json
 import os
 
@@ -8,14 +10,28 @@ app = Flask(__name__)
 
 CORS(app, supports_credentials=True, origins=['http://localhost:3000'])
 
-# Comment
-
-
-@app.route('/api/test', methods=['GET']) 
+# @app.route('/api/test', methods=['GET']) 
 
 @app.route('/test')
 def test():
     return jsonify({'message': 'Hello World!'})
+
+@app.route('/factcheck')
+def run_factcheck_go():
+    try:
+        formatted_arg = ['-queries', 'JD Vance Couch,Kamala Harris Rich']
+        result = subprocess.run(['./main.exe'] + formatted_arg, capture_output=True, text=True, encoding='utf-8')
+
+        print(result.stdout)
+        
+        if result.returncode == 0:
+            return jsonify(json.loads(result.stdout))
+        else:
+            return jsonify({'error': result.stderr}), 500
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
