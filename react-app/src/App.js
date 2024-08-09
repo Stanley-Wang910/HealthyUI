b211d509013150b7000405c69271c2667512f745
Carly Kronish <carlykro@gmail.com>
2024-08-09 14:59:46 -0400

    Use the correct endpoint
    trying to get the youtube api working to display and play videos, having trouble with the correct endpoint for youtube api data
     Please enter the commit message for your changes. Lines starting

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -7,6 +7,10 @@ import Header from "./components/Header.js";
 
 function App() {
   // Simple Effect to test API Configuration
+
+  const [videos, setVideos] = useState([]);
+  const [testData, setTestData] = useState(null);
+
   useEffect(() => {
     const testAPI = async () => {
       try {
@@ -24,11 +28,27 @@ function App() {
     testAPI();
   }, []);
 
+  useEffect(() => {
+    const fetchVideos = async () => {
+      try {
+        const response = await axios.get("http://localhost:5000/api/youtube"); //end point may change
+        if (response.status === 200) {
+          setVideos(response.data.items); 
+        } else {
+          console.error("Error with API", response.data);
+        }
+      } catch (error) {
+        console.error("Error in Videos API request:", error);
+      }
+    };
+    fetchVideos();
+  }, []);
+
   return (
     <div className="flex flex-col min-h-screen bg-white">
       {/* min-h-screen - app full height, flex-col - stack items vertically */}
       <Header />
-      <MainFeed />
+      <MainFeed videos={videos} />
     </div>
   );
 }
