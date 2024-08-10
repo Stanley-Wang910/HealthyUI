43ddff8cd1c4f829b5a18cd5201f6aa730c3b1a2
BY3D <49314704+BY3D@users.noreply.github.com>
2024-08-09 20:08:15 -0400

    Changed example to use API key instead of OAuth

--- example.py
+++ example.py
@@ -6,11 +6,7 @@
 
 import os
 
-import google_auth_oauthlib.flow
 import googleapiclient.discovery
-import googleapiclient.errors
-
-scopes = ["https://www.googleapis.com/auth/youtube.readonly"]
 
 def main():
     # Disable OAuthlib's HTTPS verification when running locally.
@@ -19,18 +15,14 @@ def main():
 
     api_service_name = "youtube"
     api_version = "v3"
-    client_secrets_file = "YOUR_CLIENT_SECRET_FILE.json" # Not relevant here
+    DEVELOPER_KEY = "Private Developer Key"
 
-    # Get credentials and create an API client
-    flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(
-        client_secrets_file, scopes)
-    credentials = flow.run_console()
     youtube = googleapiclient.discovery.build(
-        api_service_name, api_version, credentials=credentials)
+        api_service_name, api_version, developerKey = DEVELOPER_KEY)
 
     request = youtube.channels().list(
         part="snippet,contentDetails,statistics",
-        forUsername="sncf"
+        forUsername="bbcearth"
     )
     response = request.execute()
 
