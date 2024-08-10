816d79070907bd2adc3f1cbeda8cd876a886b897
Stanley Wang <wangstanley910@gmail.com>
2024-08-10 12:44:53 -0400

    blah blah

--- go/youtube.go
+++ go/youtube.go
@@ -5,14 +5,15 @@ import (
 	"io"
 	"net/http"
 	"os"
+	// "sync"
 )
 
 func youtube(id string) error {
-
 	googleApiKey := os.Getenv("GOOGLE_API_KEY")
 	if googleApiKey == "" {
 		panic("GOOGLE_API_KEY not set in .env")
 	}
+
 	url := fmt.Sprintf("https://www.googleapis.com/youtube/v3/videos?part=topicDetails%%2Csnippet%%2Cstatistics&id=%s&key=%s", id, googleApiKey)
 
 	req, _ := http.NewRequest("GET", url, nil)
@@ -32,3 +33,14 @@ func youtube(id string) error {
 	fmt.Println(string(body))
 	return nil
 }
+
+// func youtubeConcurrent(ids []string) error {
+
+// 	var wg sync.WaitGroup
+
+// 	results := make(chan struct {
+// 		id   string
+// 		err  error
+// 		body string
+
+// 	})
