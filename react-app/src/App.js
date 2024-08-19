fed009d4f1155742d24246a0799e4bec5f6683e3
Stanley Wang <wangstanley910@gmail.com>
2024-08-19 14:30:50 -0400

    Clean up floating static files

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import './App.css'
 
 import MainFeed from './components/MainFeed'
@@ -6,7 +6,20 @@ import Header from './components/Header'
 import Co2CounterWidget from './components/widget/Co2CounterWidget'
 import { VideoOpenProvider } from './context/context'
 
+const apiHost = process.env.REACT_APP_BACKEND_HOST ?? 'http://127.0.0.1:5000/'
+
 function App() {
+  useEffect(() => {
+    fetch(`${apiHost}/yt/fc?ids=bNH16A4f5Yk`)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data)
+      })
+      .catch((error) => {
+        console.error('Error fetching data:', error)
+      })
+  }, [])
+
   return (
     <>
       <VideoOpenProvider>
